5b48b3960a3b7f41420ca2632ad1ed5aa420ca26
Pavan Kumar <pavankkarkera480@gmail.com>
2025-09-12 08:14:39 +0530

    Add sumOfNaturalNumbers import and export to index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // const factorial = require("./problems/factorial");
 import factorial from "./problems/factorial.js";
+import sumOfNaturalNumbers from './problems/sum-natural-numbers.js'
+
 
 // module.exports = {
 //     factorial
 // }
 
 export {
-    factorial
+    factorial,
+    sumOfNaturalNumbers
 }
